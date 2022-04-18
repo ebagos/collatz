bb4711e68e1fd32d8c688fdcc3f80b16fdbf94d2
ebagos <sogabehideki@gmail.com>
2022-04-18 18:16:18 +0900

    add spring

---
+++ spring/literal/src/main/java/com/github/ebagos/collatz/literal/LiteralApplication.java
@@ -0,0 +1,64 @@
+package com.github.ebagos.collatz.literal;
+
+import org.lognet.springboot.grpc.GRpcService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.grpc.stub.StreamObserver;
+
+@SpringBootApplication
+public class LiteralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteralApplication.class, args);
+	}
+
+	@GRpcService
+	public static class CalculatorService extends CalculatorGrpc.CalculatorImplBase {
+		@Override
+		public void calcCollatz(CollatzRequest req, StreamObserver<CollatzResponse> resObserver) {
+			Long index_from = req.getIndexFrom();
+			Long index_to = req.getIndexTo();
+			Pair pair = collatzLoop(index_from, index_to);
+			CollatzResponse res;
+			res = CollatzResponse.newBuilder().setIndex(pair.second).build();
+			res = CollatzResponse.newBuilder().setMaxLength(pair.first).build();
+			resObserver.onNext(res);
+			resObserver.onCompleted();
+		}
+
+		public static class Pair {
+			Long first;
+			Long second;
+		}
+
+		private static Long collatz(Long n) {
+			Long count = 1L;
+			while (n > 1L) {
+				if (n % 2L == 0L) {
+					n /= 2L;
+				} else {
+					n = n * 3L + 1L;
+				}
+				count++;
+			}
+			return count;
+		}
+
+		private static Pair collatzLoop(Long s, Long e) {
+			Long max = 0L;
+			Long index = 0L;
+			for (Long i = s	; i < e; i++) {
+				Long rc = collatz(i);
+				if (rc > max) {
+					max = rc;
+					index = i;
+				}
+			}
+			Pair result = new Pair();
+			result.first = max;
+			result.second = index;
+			return result;
+		}
+	}
+}
--- spring/literal/src/test/java/com/github/ebagos/collatz/literal/LiteralApplicationTests.java
+++ spring/literal/src/test/java/com/github/ebagos/collatz/literal/LiteralApplicationTests.java
@@ -1,4 +1,4 @@
-package com.github.egabos.collatz.literal;
+package com.github.ebagos.collatz.literal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
