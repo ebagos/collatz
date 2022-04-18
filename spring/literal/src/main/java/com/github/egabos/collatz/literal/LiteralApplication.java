451e50d1655dc2499f4ae32adcf4bc321df38e26
ebagos <sogabehideki@gmail.com>
2022-04-18 11:59:35 +0900

    spring-initializr

---
+++ spring/literal/src/main/java/com/github/egabos/collatz/literal/LiteralApplication.java
@@ -0,0 +1,13 @@
+package com.github.egabos.collatz.literal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteralApplication.class, args);
+	}
+
+}
---
+++ spring/literal/src/test/java/com/github/egabos/collatz/literal/LiteralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.egabos.collatz.literal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
